6f090b7391d6fad54fbb91ef81926e870a0c47fb
Wei Liu <liuwei_berkeley@berkeley.edu>
2023-10-01 05:44:22 +0000

    added scripts for changing link type between IB and Eth.

---
+++ scripts/Eth_to_IB.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [ ! $1] ;then
+	echo "Please specify the PCIe address of the NIC!"
+	exit
+fi
+
+mstconfig -d 43:00.0 set LINK_TYPE_P1=1
+mstfwreset -d 43:00.0 -l3 -y reset
+service opensmd start
---
+++ scripts/IB_to_Eth.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [ ! $1] ;then
+	echo "Please specify the PCIe address of the NIC!"
+	exit
+fi
+
+service opensmd stop
+mstconfig -d $1 set LINK_TYPE_P1=2
+mstfwreset -d $1 -l3 -y reset
