b945281231879c53d73b1d130c2169807905fc65
Wei Liu <liuwei_berkeley@berkeley.edu>
2023-10-06 02:53:16 +0000

    added nic_reset.sh.

---
+++ scripts/nic_reset.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+dev='mlx5_0'
+
+mlxfwreset -d $dev -t0 -y reset
--- scripts/set_link_speed.sh
+++ scripts/set_link_speed.sh
@@ -1,28 +1,31 @@
 #! /bin/bash
 
+eth='ens6np0'
+dev='mlx5_0'
+
 if [ ! $1 ] ;then
 	echo "Please specify link speed: --100g or --200g or --400g"
 	exit
 fi
 
 if [ $1 == '--100g' ] ;then
-    ethtool -s ens6np0 speed 100000 autoneg on
+    ethtool -s $eth speed 100000 autoneg on
     sleep 1
-    mlxlink -d mlx5_0 |grep "Speed       "
+    mlxlink -d $dev |grep "Speed       "
 fi
 
 if [ $1 == '--200g' ] ;then
-    ethtool -s ens6np0 speed 200000 autoneg on
+    ethtool -s $eth speed 200000 autoneg on
     sleep 1
-    mlxlink -d mlx5_0 |grep "Speed       "
+    mlxlink -d $dev |grep "Speed       "
 fi
 
 if [ $1 == '--400g' ] ;then
-    ethtool -s ens6np0 speed 400000 autoneg on
+    ethtool -s ${eth} speed 400000 autoneg on
     sleep 1
-    mlxlink -d mlx5_0 |grep "Speed       "
+    mlxlink -d ${dev} |grep "Speed       "
 fi
 
 if [ $1 == '--check' ] ;then
-    mlxlink -d mlx5_0 |grep "Speed       "
+    mlxlink -d $dev |grep "Speed       "
 fi
