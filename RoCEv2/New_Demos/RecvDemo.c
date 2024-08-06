48863bc0b733c03ece2a435e4c40a4bfbbac08d9
Wei Liu <liuwei_berkeley@berkeley.edu>
2024-08-06 12:45:44 -0700

    use long options for mac,ip and port settings.

--- RoCEv2/New_Demos/RecvDemo.c
+++ RoCEv2/New_Demos/RecvDemo.c
@@ -50,6 +50,12 @@ void print_helper()
     printf("Options:\n");
     printf("    -h, print out the helper information.\n");
     printf("    -d, NIC dev number. '0' means mlx5_0.\n");
+    printf("    --smac, source MAC address.\n");
+    printf("    --dmac, destination MAC address.\n");
+    printf("    --sip, source IP address.\n");
+    printf("    --dip, destination IP address.\n");
+    printf("    --sport, source port number.\n");
+    printf("    --dport, destination port number.\n");
     printf("    --gpu, allocate memory on GPU. the memory is allocated on the host by default.\n");
 }
 
@@ -62,12 +68,12 @@ void parse_args(struct args *args, int argc, char *argv[])
     int long_option_index = -1;
     uint8_t tmp[4];
     struct option long_options[] = {
-        {.name = "smac", .has_arg = required_argument, .flag = NULL, .val = 'S'},
-        {.name = "dmac", .has_arg = required_argument, .flag = NULL, .val = 'D'},
-        {.name = "sip", .has_arg = required_argument, .flag = NULL, .val = 's'},
-        {.name = "dip", .has_arg = required_argument, .flag = NULL, .val = 'd'},
-        {.name = "sport", .has_arg = required_argument, .flag = NULL, .val = 'p'},
-        {.name = "dport", .has_arg = required_argument, .flag = NULL, .val = 'P'},
+        {.name = "smac", .has_arg = required_argument, .flag = NULL, .val = 256},
+        {.name = "dmac", .has_arg = required_argument, .flag = NULL, .val = 257},
+        {.name = "sip", .has_arg = required_argument, .flag = NULL, .val = 258},
+        {.name = "dip", .has_arg = required_argument, .flag = NULL, .val = 259},
+        {.name = "sport", .has_arg = required_argument, .flag = NULL, .val = 260},
+        {.name = "dport", .has_arg = required_argument, .flag = NULL, .val = 261},
         {.name = "gpu", .has_arg = no_argument, .flag = NULL, .val = 'g'},
         {.name = "help", .has_arg = no_argument, .flag = NULL, .val = 'h'},
         {0, 0, 0, 0}
@@ -75,10 +81,13 @@ void parse_args(struct args *args, int argc, char *argv[])
     while(1)
     {
         //c = getopt_long(argc, argv, "S:D:s:d:p:P:gh", long_options, &long_option_index);
-        c = getopt_long(argc, argv, "S:D:s:d:p:P:gh", long_options, &long_option_index);
+        c = getopt_long(argc, argv, "d:gh", long_options, &long_option_index);
         switch (c)
         {
-            case 'S':
+            case 'd':
+                sscanf(optarg, "%d", &args->device_id);
+                break;
+            case 256:
                 sscanf(optarg, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", 
                                 &args->pkt_info.src_mac[0], 
                                 &args->pkt_info.src_mac[1],
@@ -87,7 +96,7 @@ void parse_args(struct args *args, int argc, char *argv[])
                                 &args->pkt_info.src_mac[4],
                                 &args->pkt_info.src_mac[5]);
                 break;
-            case 'D':
+            case 257:
                 sscanf(optarg, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
                                 &args->pkt_info.dst_mac[0],
                                 &args->pkt_info.dst_mac[1],
@@ -96,18 +105,18 @@ void parse_args(struct args *args, int argc, char *argv[])
                                 &args->pkt_info.dst_mac[4],
                                 &args->pkt_info.dst_mac[5]);
                 break;
-            case 's':       
+            case 258:       
                 sscanf(optarg, "%hhd.%hhd.%hhd.%hhd", &tmp[0], &tmp[1], &tmp[2], &tmp[3]);
                 args->pkt_info.src_ip = (tmp[3] << 24) | (tmp[2] << 16) | (tmp[1] << 8) | tmp[0];
                 break;
-            case 'd':
+            case 259:
                 sscanf(optarg, "%hhd.%hhd.%hhd.%hhd", &tmp[0], &tmp[1], &tmp[2], &tmp[3]);
                 args->pkt_info.dst_ip = (tmp[3] << 24) | (tmp[2] << 16) | (tmp[1] << 8) | tmp[0];
                 break;
-            case 'p':
+            case 260:
                 sscanf(optarg, "%hd", &args->pkt_info.src_port);
                 break;
-            case 'P':
+            case 261:
                 sscanf(optarg, "%hd", &args->pkt_info.dst_port);
                 break;
             case 'g':
@@ -120,7 +129,7 @@ void parse_args(struct args *args, int argc, char *argv[])
             default:
                 printf("Invalid option.\n");
                 print_helper();
-                break;
+                return;
         }
     }
 }
@@ -130,7 +139,7 @@ Print out device information.
 */
 void print_dev_info(struct args *args){
     printf("Device information:\n");
-    printf("    device_id: %d\n", device_id);
+    printf("    device_id: %d\n", args->device_id);
     printf("    src_mac: %02x:%02x:%02x:%02x:%02x:%02x\n", 
                 args->pkt_info.src_mac[0], 
                 args->pkt_info.src_mac[1], 
